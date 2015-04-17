626d73f2ae2e38d6fa2a78d06e567473ebd44d99
firefreak11 <dave.yognaught113@gmail.com>
2015-04-16 20:12:09 -0400

    Update Powder.java

--- src/Powder.java
+++ src/Powder.java
@@ -1,5 +1,5 @@
 /**
- *	@Copyright 2015 Chris Beimers/firefreak11
+ *	@Copyright 2015 firefreak11
  *
  *	This file is part of PowderInJava.
  *
@@ -19,255 +19,252 @@
 
 package powderinjava;
 
-import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
+import java.util.ArrayList;
 import java.util.Iterator;
-
+import java.util.List;
 import powderinjava.elements.Element;
 
-public class Powder extends Engine {
+public class Powder extends Engine{
 
 	/**
-	 * Bean Powder, a Java recreation of The Powder Toy
+	 * Powder In Java, a Java recreation of The Powder Toy
 	 * 
 	 * @link http://powdertoy.co.uk
-	 * @author Chris Beimers/firefreak11
+	 * @author firefreak11
 	 * @date April 12, 2015
 	 */
 
-	// SIMULATION
+	// Main Simulation Mechanics
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID=1L;
 
-	private Element spawnType;
+	public static int xMarginLeft=10;
+
+	public static int xMarginRight(){
+		try{
+			return Main.powder.width-xMarginLeft;
+		}catch(NullPointerException e){
+			return 0;
+		}
+	}
+
+	public static int yMarginTop=10;
+
+	public static int yMarginBottom(){
+		try{
+			return Main.powder.height-(yMarginTop<<2)-1;
+		}catch(NullPointerException e){
+			return 0;
+		}
+	}
+
+	public Menu menu;
+	public VelocityMap v;
+	public Element spawnType;
 
 	private int mx;
 	private int my;
 	private int cursorRadius;
 
-	private boolean glowing;
+	public boolean fancyGraphics;
 	private boolean spawning;
 	private boolean erasing;
+	private boolean paused;
 
-	public Powder() {
-		super("Powder In Java", 650, 475, false, false);
-		spawnType = Element.AIR;
+	public Powder(){
+		super("Powder In Java",650,475,false,false);
 	}
 
-	public void update() {
-
-	}
-
-	public void render() {
-		if (spawning && !erasing)
-			fillCursor(cursorRadius);
-		if (!spawning && erasing)
-			eraseCursor(cursorRadius);
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				img.setRGB(x, y, 0xff000000);
-			}
+	public void tick(){
+		if(spawning&&!erasing) fillCursor(cursorRadius);
+		if(!spawning&&erasing) eraseCursor(cursorRadius);
+		v.update();
+		for(Iterator<Particle> iterator=Particle.particles.iterator();iterator.hasNext();){
+			Particle particle=iterator.next();
+			if(!paused) particle.update();
 		}
-		for (Iterator<Particle> iterator = Particle.particles.iterator(); iterator
-				.hasNext();) {
-			Particle particle = iterator.next();
-			if (particle.removeQueue) {
-				iterator.remove();
-				return;
-			} else {
-				particle.update();
-				int x = particle.x;
-				int y = particle.y;
-				Color baseColour = particle.element.colour;
-				if (glowing) {
-					Color glow = new Color(baseColour.getRed() / 2,
-							baseColour.getGreen() / 2,
-							baseColour.getBlue() / 2, baseColour.getAlpha() / 2);
-					for (int i = 0; i < 4; i++)
-						try {
-							int ax = i == 0 ? x - 1 : i == 1 ? x + 1 : x;
-							int ay = i == 2 ? y - 1 : i == 3 ? y + 1 : y;
-							img.setRGB(ax, ay * width, glow.getRGB());
-						} catch (ArrayIndexOutOfBoundsException e) {
-							continue;
-						}
-				}
-				try {
-					img.setRGB(x, y * width, baseColour.getRGB());
-				} catch (ArrayIndexOutOfBoundsException e) {
-					continue;
-				}
-			}
-		}
-		drawCursor(mx, my, cursorRadius);
+		drawCursor(mx,my,cursorRadius);
 	}
 
-	public void spawnParticle(int x, int y, Element spawnType) {
-		if (Particle.particleAt(x, y) != null
-				|| Particle.particles.size() >= 1000)
-			return;
-		new Particle(x, y, spawnType);
+	public void spawnParticle(int x,int y,Element spawnType){
+		if(Particle.particleAt(x,y)!=null) return;
+		new Particle(x,y,spawnType);
 	}
 
-	public void save() {
+	public void save(){
 
 	}
 
-	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_1:
-			glowing = true;
-			break;
-		case KeyEvent.VK_2:
-			glowing = false;
-			break;
+	public void keyPressed(KeyEvent e){
+		switch(e.getKeyCode()){
+			case KeyEvent.VK_SPACE:
+				paused=!paused;
+				break;
+			case KeyEvent.VK_1:
+				fancyGraphics=!fancyGraphics;
+				break;
 		}
 	}
 
-	public void keyReleased(KeyEvent e) {
+	public void keyReleased(KeyEvent e){
 
 	}
 
-	public void keyTyped(KeyEvent e) {
+	public void keyTyped(KeyEvent e){
 
 	}
 
-	public void mouseClicked(MouseEvent e) {
+	public void mouseClicked(MouseEvent e){
 
 	}
 
-	public void mouseEntered(MouseEvent e) {
+	public void mouseEntered(MouseEvent e){
 
 	}
 
-	public void mouseExited(MouseEvent e) {
+	public void mouseExited(MouseEvent e){
 
 	}
 
-	public void mousePressed(MouseEvent e) {
-		if (e.getButton() == MouseEvent.BUTTON1)
-			spawning = true;
-		else if (e.getButton() == MouseEvent.BUTTON3)
-			erasing = true;
+	public void mousePressed(MouseEvent e){
+		if(e.getButton()==MouseEvent.BUTTON1){
+			spawning=true;
+			erasing=false;
+		}else if(e.getButton()==MouseEvent.BUTTON3){
+			erasing=true;
+			spawning=false;
+		}
+		menu.grabMouse(mx,my,true);
 	}
 
-	public void mouseReleased(MouseEvent e) {
-		spawning = false;
-		erasing = false;
+	public void mouseReleased(MouseEvent e){
+		spawning=false;
+		erasing=false;
+		menu.grabMouse(mx,my,false);
 	}
 
-	public void mouseDragged(MouseEvent e) {
-		mx = e.getX();
-		my = e.getY();
+	public void mouseDragged(MouseEvent e){
+		mx=e.getX();
+		my=e.getY();
+		menu.grabMouse(mx,my,true);
 	}
 
-	public void mouseMoved(MouseEvent e) {
-		mx = e.getX();
-		my = e.getY();
+	public void mouseMoved(MouseEvent e){
+		mx=e.getX();
+		my=e.getY();
+		menu.grabMouse(mx,my,false);
 	}
 
-	public void mouseWheelMoved(MouseWheelEvent e) {
-		int n = e.getWheelRotation();
-		if (n < 0 && cursorRadius < 50)
-			cursorRadius++;
-		if (n > 0 && cursorRadius > 0)
-			cursorRadius--;
+	public void mouseWheelMoved(MouseWheelEvent e){
+		int n=e.getWheelRotation();
+		if(n<0&&cursorRadius<50) cursorRadius++;
+		if(n>0&&cursorRadius>0) cursorRadius--;
 	}
 
-	public void fillCursor(int radius) {
-		if (radius == 0) {
-			spawnParticle(mx, my, spawnType);
+	public void fillCursor(int radius){
+		if(radius==0){
+			spawnParticle(mx,my,spawnType);
 			return;
 		}
-		int x = radius;
-		int y = 0;
-		int radiusError = 1 - x;
+		int x=radius;
+		int y=0;
+		int radiusError=1-x;
 
-		while (x >= y) {
-			spawnParticle(x + mx, y + my, spawnType);
-			spawnParticle(y + mx, x + my, spawnType);
-			spawnParticle(-x + mx, y + my, spawnType);
-			spawnParticle(-y + mx, x + my, spawnType);
-			spawnParticle(-x + mx, -y + my, spawnType);
-			spawnParticle(-y + mx, -x + my, spawnType);
-			spawnParticle(x + mx, -y + my, spawnType);
-			spawnParticle(y + mx, -x + my, spawnType);
+		while(x>=y){
+			spawnParticle(x+mx,y+my,spawnType);
+			spawnParticle(y+mx,x+my,spawnType);
+			spawnParticle(-x+mx,y+my,spawnType);
+			spawnParticle(-y+mx,x+my,spawnType);
+			spawnParticle(-x+mx,-y+my,spawnType);
+			spawnParticle(-y+mx,-x+my,spawnType);
+			spawnParticle(x+mx,-y+my,spawnType);
+			spawnParticle(y+mx,-x+my,spawnType);
 			y++;
-			if (radiusError < 0) {
-				radiusError += 2 * y + 1;
-			} else {
+			if(radiusError<0){
+				radiusError+=2*y+1;
+			}else{
 				x--;
-				radiusError += 2 * (y - x) + 1;
+				radiusError+=2*(y-x)+1;
 			}
 		}
 		fillCursor(--radius);
 	}
 
-	public void eraseCursor(int radius) {
-		if (radius == 0) {
-			spawnParticle(mx, my, spawnType);
+	public void eraseCursor(int radius){
+		if(radius<=0) try{
+			Particle.particleAt(mx,my).remove();
+			return;
+		}catch(NullPointerException e){
 			return;
 		}
-		int x = radius;
-		int y = 0;
-		int radiusError = 1 - x;
-
-		while (x >= y) {
-			Particle[] toRemove = new Particle[8];
-			toRemove[0] = Particle.particleAt(x + mx, y + my);
-			toRemove[1] = Particle.particleAt(y + mx, x + my);
-			toRemove[2] = Particle.particleAt(-x + mx, y + my);
-			toRemove[3] = Particle.particleAt(-y + mx, x + my);
-			toRemove[4] = Particle.particleAt(-x + mx, -y + my);
-			toRemove[5] = Particle.particleAt(-y + mx, -x + my);
-			toRemove[6] = Particle.particleAt(x + mx, -y + my);
-			toRemove[7] = Particle.particleAt(y + mx, -x + my);
-			for (Particle p : toRemove)
-				if (p == null)
+		int x=radius;
+		int y=0;
+		int radiusError=1-x;
+		int mx=this.mx;
+		int my=this.my;
+		List<Particle> toRemove=new ArrayList<Particle>();
+		while(x>=y){
+			toRemove.add(Particle.particleAt(x+mx,y+my));
+			toRemove.add(Particle.particleAt(y+mx,x+my));
+			toRemove.add(Particle.particleAt(-x+mx,y+my));
+			toRemove.add(Particle.particleAt(-y+mx,x+my));
+			toRemove.add(Particle.particleAt(-x+mx,-y+my));
+			toRemove.add(Particle.particleAt(-y+mx,-x+my));
+			toRemove.add(Particle.particleAt(x+mx,-y+my));
+			toRemove.add(Particle.particleAt(y+mx,-x+my));
+			y++;
+			if(radiusError<0){
+				radiusError+=2*y+1;
+			}else{
+				x--;
+				radiusError+=2*(y-x)+1;
+			}
+		}
+		for(Iterator<Particle> iterator=Particle.particles.iterator();iterator.hasNext();){
+			Particle current=iterator.next();
+			for(Particle m:toRemove){
+				try{
+					if(m.equals(current))
+						iterator.remove();
+				}catch(NullPointerException e){
 					continue;
-				else
-					p.remove();
-			y++;
-			if (radiusError < 0) {
-				radiusError += 2 * y + 1;
-			} else {
-				x--;
-				radiusError += 2 * (y - x) + 1;
+				}
 			}
 		}
-		fillCursor(--radius);
+		eraseCursor(--radius);
 	}
 
-	public void drawCursor(int x0, int y0, int radius) {
-		int x = radius;
-		int y = 0;
-		int radiusError = 1 - x;
+	public void drawCursor(int x0,int y0,int radius){
+		int x=radius;
+		int y=0;
+		int radiusError=1-x;
 
-		while (x >= y) {
-			drawPixel(x + x0, y + y0, 0xffaaaaaa);
-			drawPixel(y + x0, x + y0, 0xffaaaaaa);
-			drawPixel(-x + x0, y + y0, 0xffaaaaaa);
-			drawPixel(-y + x0, x + y0, 0xffaaaaaa);
-			drawPixel(-x + x0, -y + y0, 0xffaaaaaa);
-			drawPixel(-y + x0, -x + y0, 0xffaaaaaa);
-			drawPixel(x + x0, -y + y0, 0xffaaaaaa);
-			drawPixel(y + x0, -x + y0, 0xffaaaaaa);
+		while(x>=y){
+			drawPixel(x+x0,y+y0,0xffaaaaaa);
+			drawPixel(y+x0,x+y0,0xffaaaaaa);
+			drawPixel(-x+x0,y+y0,0xffaaaaaa);
+			drawPixel(-y+x0,x+y0,0xffaaaaaa);
+			drawPixel(-x+x0,-y+y0,0xffaaaaaa);
+			drawPixel(-y+x0,-x+y0,0xffaaaaaa);
+			drawPixel(x+x0,-y+y0,0xffaaaaaa);
+			drawPixel(y+x0,-x+y0,0xffaaaaaa);
 			y++;
-			if (radiusError < 0) {
-				radiusError += 2 * y + 1;
-			} else {
+			if(radiusError<0){
+				radiusError+=2*y+1;
+			}else{
 				x--;
-				radiusError += 2 * (y - x) + 1;
+				radiusError+=2*(y-x)+1;
 			}
 		}
 	}
 
-	public void drawPixel(int x, int y, int colour) {
-		try {
-			img.setRGB(x, y, colour);
-		} catch (ArrayIndexOutOfBoundsException e) {
+	public void drawPixel(int x,int y,int colour){
+		try{
+			img.setRGB(x,y,colour);
+		}catch(ArrayIndexOutOfBoundsException e){
 			return;
 		}
 	}
