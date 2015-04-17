87a376205f9c9ff264ace3a1140bf73516cd2c60
firefreak11 <dave.yognaught113@gmail.com>
2015-04-16 20:12:27 -0400

    Update Particle.java

--- src/Particle.java
+++ src/Particle.java
@@ -1,5 +1,5 @@
 /**
- *	@Copyright 2015 Chris Beimers/firefreak11
+ *	@Copyright 2015 firefreak11
  *
  *	This file is part of PowderInJava.
  *
@@ -23,133 +23,135 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
-
 import powderinjava.elements.Element;
 
 /**
  * A Specific Particle On Screen
  */
 
-public class Particle {
+public class Particle{
 
-	public static List<Particle> particles = new ArrayList<Particle>();
+	public static List<Particle> particles=new ArrayList<Particle>();
 
 	public Element element;
 	private Random rand;
 
 	public int x;
 	public int y;
-	public int dx;
-	public int dy;
+	public int vx;
+	public int vy;
 	public int t;
 
 	public boolean removeQueue;
 
-	public Particle(int x, int y, Element element) {
-		this.x = x;
-		this.y = y;
-		this.element = element;
-		rand = new Random();
-		switch (element.state) {
-		case SOLID:
-			dx = dy = 0;
-			break;
-		case LIQUID:
-			dx = rand.nextInt(3) - 1;
-			dy = 1;
-			break;
-		case GAS:
-			dx = rand.nextInt(3) - 1;
-			dy = rand.nextInt(3) - 1;
-			break;
-		case POWDER:
-			dx = 0;
-			dy = 1;
-			break;
+	public Particle(int x,int y,Element element){
+		this.x=x;
+		this.y=y;
+		this.element=element;
+		rand=new Random();
+		switch(element.state){
+			case SOLID:
+				vx=vy=0;
+				break;
+			case LIQUID:
+				vx=rand.nextInt(3)-1;
+				vy=1;
+				break;
+			case GAS:
+				vx=rand.nextInt(3)-1;
+				vy=rand.nextInt(3)-1;
+				break;
+			case POWDER:
+				vx=0;
+				vy=1;
+				break;
+			case SPECIAL:
+				vx=vy=0;
+				break;
+			case QUANTUM:
+				vx=rand.nextInt(2)==0?1:-1;
+				vy=rand.nextInt(2)==0?1:-1;
+				break;
 		}
-		this.t = 0;
 		particles.add(this);
 	}
 
-	public synchronized void update() {
-		if (++t % 100 == 0) {
-			switch (element.state) {
-			case GAS:
-				if (particleAt(x + dx, y + dy) == null) {
-					dx += rand.nextInt(3) - 1;
-					dy += rand.nextInt(3) - element.mass;
-					if(dx==0)dx=rand.nextInt(2)==0?1:-1;
-					if(dy==0)dy=rand.nextInt(2)==0?1:-1;
-					x += dx;
-					y -= dy;
-				}
-				break;
-			case LIQUID:
-				if (particleAt(x, y + 1) == null)
-					y += dy;
-				if (particleAt(x + dx, y) == null) {
-					x += dx;
-					dx += rand.nextInt(3) - 1;
-				}
-				break;
-			case POWDER:
-				break;
-			case SOLID:
-				break;
+	public synchronized void update(){
+		if(removeQueue)return;
+		if(!element.state.equals(State.SOLID))if(++t%1/*(25-element.mass/4)*/==0){
+			switch(element.state){
+				case GAS:
+					displace(vx*(rand.nextInt(3)-1),vy*(rand.nextInt(3)-1));
+					vx=rand.nextInt(3)-1;
+					vy=rand.nextInt(3)-1;
+					break;
+				case LIQUID:
+					displace(vx*(rand.nextInt(5)-2),vy*rand.nextInt(2));
+					vx=rand.nextInt(3)-1;
+					break;
+				case POWDER:
+					displace(vx*(rand.nextInt(3)-1),vy);
+					break;
+				case QUANTUM:
+					displace(vx,vy);
+					break;
+				default:
+					break; // Don't need to update movement for solids.
 			}
-			for (int i = 0; i < 8; i++) {
-				int ax = i < 3 ? x - 1 : i < 4 ? x : x + 1;
-				int ay = i == 0 || i == 3 || i == 5 ? y + 1
-						: i == 1 || i == 6 ? y : y - 1;
-				element.update(ax, ay);
-			}
-			if (x < 0 || y < 0 || x >= Main.powder.width
-					|| y >= Main.powder.height)
-				remove();
-			while (dx > 1)
-				dx--;
-			while (dx < -1)
-				dx++;
-			while (dy > 1)
-				dy--;
-			while (dy < -1)
-				dy++;
-			t = 0;
+			t=0;
 		}
-		if (particleAt(x, y) != null)
-			if (x < this.x) {
-				displace(this.x + 1, this.y);
-				if (y < this.y)
-					displace(this.x, this.y + 1);
-				else if (y > this.y)
-					displace(this.x, this.y - 1);
-			} else if (x > this.x) {
-				displace(this.x - 1, this.y);
-				if (y < this.y)
-					displace(this.x, this.y + 1);
-				else if (y > this.y)
-					displace(this.x, this.y - 1);
-			}
+		for(int i=0;i<8;i++){
+			int ax=i<3?x-1:i<4?x:x+1;
+			int ay=i==0||i==3||i==5?y+1:i==1||i==6?y:y-1;
+			element.update(ax,ay,this); // Checks and updates for elemental behaviour surrounding this particle
+		}
+		if(x<0||y<0||x>Main.powder.width||y>Main.powder.height) remove();
 	}
 
-	public void displace(int dx, int dy) {
-		if (particleAt(x + dx, y + dy) == null) {
-			x += dx;
-			y += dy;
+	public void displace(int xDest,int yDest){
+		if(x+xDest==Powder.xMarginLeft||y+yDest==Powder.yMarginTop||x+xDest==Powder.xMarginRight()||y+yDest==Powder.yMarginBottom()){// Collide with the borders
+			return;
 		}
+		if(x+xDest<Powder.xMarginLeft||y+yDest<Powder.yMarginTop||x+xDest>Powder.xMarginRight()||y+yDest>Powder.yMarginBottom()){// If it is past the borders, remove it.
+			 remove();
+			return;
+		}
+		Particle check=particleAt(x+xDest,y+yDest);
+		if(check!=null){
+			if(check.element.mass>=element.mass){
+				if(check.element.equals(element)){
+					if(element.stacks) return;
+					if(particleAt(x-1,y+1)==null){
+						displace(-1,1);
+						return;
+					}
+					if(particleAt(x+1,y+1)==null){
+						displace(1,1);
+						return;
+					}
+				}
+				return;
+			}
+			// check.displace(rand.nextInt(2)==0?1:-1,-1);
+			if(rand.nextInt(50)<=10) check.y--;
+		}
+		int px=x;
+		int py=y;
+		x+=xDest+Main.powder.v.vx[px][py];
+		y+=yDest+Main.powder.v.vy[px][py];
+		if(rand.nextInt(100)<=25)Main.powder.v.vx[px][py]+=xDest;
+		if(rand.nextInt(100)<=25)Main.powder.v.vy[px][py]+=yDest;
 	}
 
-	public static Particle particleAt(int x, int y) {
-		for (Iterator<Particle> iterator = particles.iterator(); iterator
-				.hasNext();) {
-			Particle particle = iterator.next();
-			if (particle.x == x && particle.y == y)
-				return particle;
+	public static Particle particleAt(int x,int y){
+		for(Iterator<Particle> iterator=particles.iterator();iterator.hasNext();){
+			Particle particle=iterator.next();
+			if(particle.x==x&&particle.y==y) return particle;
 		}
 		return null;
 	}
 
-	public void remove() {
-		removeQueue = true;
+	public void remove(){
+		removeQueue=true;
 	}
 }
