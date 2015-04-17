81076e79ad2aa1407ee240f251c18d31b891b387
firefreak11 <dave.yognaught113@gmail.com>
2015-04-16 20:15:09 -0400

    Create NONE.java

---
+++ src/elements/NONE.java
@@ -0,0 +1,35 @@
+/**
+ *	@Copyright 2015 firefreak11
+ *
+ *	This file is part of PowderInJava.
+ *
+ *	PowderInJava is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	PowderInJava is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with PowderInJava.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+package powderinjava.elements;
+
+import powderinjava.Particle;
+import powderinjava.State;
+
+public class NONE extends Element{
+
+	/**A container for erasing, to avoid NullPointerException*/
+	public NONE() {
+		super("NONE", State.SOLID, 0x00000000, 100, false, false, false);
+	}
+
+	public void update(int x, int y, Particle p) {
+		
+	}
+}
