c971d123e755372c928d963da2afaceff8bbcf79
firefreak11 <dave.yognaught113@gmail.com>
2015-04-17 21:49:30 -0400

    Create HYGN.java

---
+++ src/elements/HYGN.java
@@ -0,0 +1,16 @@
+package powderinjava.elements;
+
+import powderinjava.Particle;
+import powderinjava.State;
+
+
+public class HYGN extends Element{
+
+	public HYGN(){
+		super("HYGN",State.GAS,0x0D86FF,0,true,false,false);
+	}
+	
+	public int update(int x, int y, Particle p){
+		return 0;
+	}
+}
